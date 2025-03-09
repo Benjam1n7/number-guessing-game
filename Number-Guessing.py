95a96fcd303dd0e58292a9c77c26bc031af8d308
Benjam1n7 <elevatebenjamin@gmail.com>
2025-03-10 00:18:06 +0100

    Updated what a life

--- Number-Guessing.py
+++ Number-Guessing.py
@@ -1,9 +1,9 @@
 import random
-correctnumber= random.randint(1, 100)
-print(correctnumber)
+correct_number= random.randint(1, 100)
+print(correct_number)
 guess= int(input("Guess a number between 1 and 100: "))
 print("You guessed:", guess)
-if guess == correctnumber:
+if guess == correct_number:
   print("guess correct")
 else:
   print("wrong guess")
